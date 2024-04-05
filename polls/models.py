b861d1a36d3b7b68b2bee98fc942be82cc9c9ab5
Divyanshu <divyanshukrishna11@gmail.com>
2024-04-05 23:13:17 +0530

    changes_into_models.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Category1(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    field1 = models.CharField(max_length=100)
+    field2 = models.IntegerField()
+
+class Category2(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    field1 = models.CharField(max_length=100)
+    field2 = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Category3(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    field1 = models.CharField(max_length=100)
+    field2 = models.BooleanField()
+
