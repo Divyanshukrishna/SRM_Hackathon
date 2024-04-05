9eb22f340e93833bf9cf70a287b5797c5c7eb11c
Divyanshu <divyanshukrishna11@gmail.com>
2024-04-05 23:24:04 +0530

    adding_data

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Category)
+
+admin.site.register(Category1)
+
+admin.site.register(Category2)
